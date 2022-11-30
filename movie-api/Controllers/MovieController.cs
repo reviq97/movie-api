da2fc6ad0947dfe7408cfcd90c7eb357b5c8a005
reviq97 <nway@o2.pl>
2022-11-30 23:55:20 +0100

    Init project

---
+++ movie-api/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using movie_api.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace movie_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{title}")]
+        public string Get(string title)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Movie movie)
+        {
+        }
+
+        
+    }
+}
---
+++ movie-api/Entities/Movie.cs
@@ -0,0 +1,42 @@
+﻿namespace movie_api.Entities
+{
+    public class Movie
+    {
+		private long _id;
+		private string _title;
+		private string _director;
+		private int _year;
+		private int _rate;
+
+		public int Rate
+		{
+			get { return _rate; }
+			init { _rate = value; }
+		}
+
+		public int Year
+		{
+			get { return _year; }
+			init { _year = value; }
+		}
+
+		public string Director
+		{
+			get { return _director; }
+			init { _director = value; }
+		}
+
+		public string Title
+		{
+			get { return _title; }
+			init { _title = value; }
+		}
+
+		public long Id
+		{
+			get { return _id; }
+			init { _id = value; }
+		}
+
+	}
+}
